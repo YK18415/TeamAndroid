199737de759da0dbd428594db43ac6e7ca1e2f35
Sören Koch <soerenkoch.bockenem@gmx.de>
2019-05-17 20:09:13 +0200

    Phonenumber limited: "0" to "9" and "+" (NewContact) and Street-Number changed : "0" to "9" and "a" to "g" (NewContact) and Postal Code limited : "0" to "9" / maxLength = 5 (NewContact)

--- app/src/main/java/android/ostfalia/teamandroid/NewContact.java
+++ app/src/main/java/android/ostfalia/teamandroid/NewContact.java
@@ -1,6 +1,7 @@
 package android.ostfalia.teamandroid;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;

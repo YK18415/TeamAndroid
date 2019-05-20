bf907543d2b3c6ba5841877ad530d3acf3de66c3
Yannick <yannick.radke@gmail.com>
2019-05-20 14:00:24 +0200

    added a second callActivity (Betreuer/Betreuter), managed the layout and changed strings.xml.

--- app/src/main/java/android/ostfalia/teamandroid/CallActivity.java
+++ app/src/main/java/android/ostfalia/teamandroid/CallActivity.java
@@ -8,6 +8,6 @@ public class CallActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_call);
+        setContentView(R.layout.activity_call_Betreuer);
     }
 }

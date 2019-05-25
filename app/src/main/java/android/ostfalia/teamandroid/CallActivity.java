6c710bd0d059ab45a4c03ffade85a9ca48a7f825
Yannick <yannick.radke@gmail.com>
2019-05-25 16:28:21 +0200

    added firebase support (google-services.json) and uploaded a file (taken photo) to firebase storage.

--- app/src/main/java/android/ostfalia/teamandroid/CallActivity.java
+++ app/src/main/java/android/ostfalia/teamandroid/CallActivity.java
@@ -7,10 +7,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.ActionBar;
@@ -18,6 +20,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +30,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.internal.telephony.ITelephony;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.gson.Gson;
 
 import java.io.ByteArrayOutputStream;
@@ -52,9 +64,45 @@ public class CallActivity extends AppCompatActivity {
     /*//Storage:
     SharedPreferences settings;*/
 
+    // Firebase - CloudStorage:
+    private StorageReference mStorageRef;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            // do your stuff
+        } else {
+            signInAnonymously();
+        }
+    }
+
+    private void signInAnonymously() {
+        mAuth.signInAnonymously().addOnSuccessListener(this, new  OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+               Toast.makeText(CallActivity.this, "Eingeloggt. Juhuu.", Toast.LENGTH_LONG).show();
+            }
+        })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Log.e(TAG, "signInAnonymously:FAILURE", exception);
+                    }
+                });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Firebase - CloudStorage:
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+
+
         setLayout();
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.actionbar);
@@ -182,10 +230,34 @@ public class CallActivity extends AppCompatActivity {
             if(file.exists()) {
                 bitmap = BitmapFactory.decodeFile(currentPhotoPath);
                 imageView.setImageBitmap(bitmap);
+                sendPhotoToFirebase(file);
             }
         }
     }
 
+    private void sendPhotoToFirebase(File file) {
+        Uri fileUri = Uri.fromFile(file);
+        StorageReference riversRef = mStorageRef.child("images/test.jpg");
+
+
+        riversRef.putFile(fileUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+
+                        Task<Uri> firebaseUri = taskSnapshot.getStorage().getDownloadUrl();
+
+                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        exception.printStackTrace();
+                    }
+                });
+    }
 
     // Code by google from 'https://developer.android.com/training/camera/photobasics':
     private File createImageFile() throws IOException {
